cf6b442887f26a47f7806cf4fc392fd9591caa18
Noor Azam <noor@rad.net.id>
2020-01-25 10:52:32 +0700

    Menambahkan 1. Moh Noor Al Azam (04060302)

--- ready-to-git.py
+++ ready-to-git.py
@@ -1,2 +1,4 @@
 # Tulis Nama dan NIM kamu sebagai penanda kamu sudah siap menggunakan GIT
 print("Berikut nama-nama yang sudah clone, modify, commit dan push file ini:")
+print("1. Moh Noor Al Azam (04060302)")
+print()
